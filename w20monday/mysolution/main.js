7bca0808af9ead2c5adf3c0aa5bfb968c5b6c2d8
Maria <mari4w04@stud.kea.dk>
2018-05-14 14:36:03 +0200

    small changes

---
+++ w20monday/mysolution/main.js
@@ -0,0 +1,12 @@
+let notif = document.querySelector(".notif")
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    notif.style.bottom = "-100px";
+    notif.classList.add("slide-up");
+  });
+
+document.querySelector(".close-btn").addEventListener("click", function(){
+    notif.style.bottom = "0";
+    notif.classList.remove("slide-up");
+    notif.classList.add("slide-down");
+});
